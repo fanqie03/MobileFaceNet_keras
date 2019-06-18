b8189ae4f8ef41e6ba16a583c50cd89114e1a835
mengfu188 <1242733702@qq.com>
2019-06-18 15:56:16 +0800

    update compare.py

--- compare.py
+++ compare.py
@@ -34,8 +34,57 @@ import keras
 from nets.net import prelu
 from PIL import Image
 import cv2
+import os
+import re
 import tensorflow as tf
 from pathlib import Path
+
+
+def load_model(model):
+    # Check if the model is a model directory (containing a metagraph and a checkpoint file)
+    #  or if it is a protobuf file with a frozen graph
+    model_exp = os.path.expanduser(model)
+    if (os.path.isfile(model_exp)):
+        print('Model filename: %s' % model_exp)
+        with tf.gfile.FastGFile(model_exp, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            tf.import_graph_def(graph_def, name='')
+    else:
+        print('Model directory: %s' % model_exp)
+        meta_file, ckpt_file = get_model_filenames(model_exp)
+
+        print('Metagraph file: %s' % meta_file)
+        print('Checkpoint file: %s' % ckpt_file)
+
+        saver = tf.train.import_meta_graph(os.path.join(model_exp, meta_file))
+        saver.restore(tf.get_default_session(), os.path.join(model_exp, ckpt_file))
+
+
+def get_model_filenames(model_dir):
+    files = os.listdir(model_dir)
+    meta_files = [s for s in files if s.endswith('.meta')]
+    if len(meta_files) == 0:
+        raise ValueError('No meta file found in the model directory (%s)' % model_dir)
+    elif len(meta_files) > 1:
+        raise ValueError('There should not be more than one meta file in the model directory (%s)' % model_dir)
+    meta_file = meta_files[0]
+    ckpt = tf.train.get_checkpoint_state(model_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        ckpt_file = os.path.basename(ckpt.model_checkpoint_path)
+        return meta_file, ckpt_file
+
+    meta_files = [s for s in files if '.ckpt' in s]
+    max_step = -1
+    for f in files:
+        step_str = re.match(r'(^model-[\w\- ]+.ckpt-(\d+))', f)
+        if step_str is not None and len(step_str.groups()) >= 2:
+            step = int(step_str.groups()[1])
+            if step > max_step:
+                max_step = step
+                ckpt_file = step_str.groups()[0]
+    return meta_file, ckpt_file
+
 
 def main(args):
     base_dir = Path(args.base_dir).expanduser()
@@ -47,6 +96,17 @@ def main(args):
     if args.model.endswith('.h5'):
         model = keras.models.load_model(args.model, custom_objects={'prelu': prelu})
         emb = model.predict(images)
+    elif args.model.endswith('.pb'):
+        with tf.Graph().as_default():
+            with tf.Session() as sess:
+                # Load the model
+                load_model(args.model)
+
+                # Get input and output tensors, ignore phase_train_placeholder for it have default value.
+                inputs_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+                embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+
+                emb = sess.run(embeddings, feed_dict={inputs_placeholder: images})
     else:
         interpreter = tf.lite.Interpreter(model_path=args.model)
         interpreter.allocate_tensors()
@@ -63,11 +123,11 @@ def main(args):
         # Test model on random input data.
         emb = np.empty(shape=(nrof_images, 128), dtype=np.float32)
         for i, img in enumerate(images):
-            img = img.reshape((1,112,112,3))
+            img = img.reshape((1, 112, 112, 3))
             interpreter.set_tensor(input_details[0]['index'], img)
 
             interpreter.invoke()
-            emb[i,...] = interpreter.get_tensor(output_details[0]['index']).reshape((-1,))
+            emb[i, ...] = interpreter.get_tensor(output_details[0]['index']).reshape((-1,))
 
     print('Images:')
     for i in range(nrof_images):
